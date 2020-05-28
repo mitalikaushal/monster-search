b7479c7b76e0a5c8e48ba53d58b765998f80a264
Mitali Kaushal <mitali.kaushal9@gmail.com>
2020-05-27 21:05:13 -0700

    Updates

---
+++ precache-manifest.1ea047912136722d850a98b5af713a49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44bae7c69a06c43f416c1a3955df308c",
+    "url": "/monster-search/index.html"
+  },
+  {
+    "revision": "861424ed4c3a29ff6ae2",
+    "url": "/monster-search/static/css/main.fde2c0c3.chunk.css"
+  },
+  {
+    "revision": "dc150c0b7bc03a663f43",
+    "url": "/monster-search/static/js/2.036199f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-search/static/js/2.036199f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861424ed4c3a29ff6ae2",
+    "url": "/monster-search/static/js/main.3cd279d0.chunk.js"
+  },
+  {
+    "revision": "3969b5e50b1932f5248f",
+    "url": "/monster-search/static/js/runtime-main.1174e8fe.js"
+  }
+]);
